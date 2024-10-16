59c2a6370fd58d8d7af768e73d48dae64eb0fbe2
kevin504-max <kevinbrito2012@gmail.com>
2024-10-16 17:02:55 -0300

    doc: documente useListTracks hook file with JSDocs

--- src/views/player/hooks/useListTracks.js
+++ src/views/player/hooks/useListTracks.js
@@ -1,15 +1,34 @@
 import { ref } from 'vue';
 import  { supabase } from '../../../providers/supabase';
 
-
+/**
+ * Hook para gerenciar a lista de faixas (tracks) utilizando Supabase.
+ * @returns {Object} Retorna os métodos e propriedades para gerenciar e buscar faixas.
+*/
 const useListTracks = () => {
+    /**
+     * Array reativo que armazena as faixas buscadas.
+     * @type {import('vue').Ref<Array<Object>>}
+    */
     const fetchedTracks = ref([]);
 
+    /**
+     * Manipula erros nas requisições e lança o erro após logar no console.
+     * @param {string} action - Nome da ação que resultou no erro.
+     * @param {Error} error - Objeto de erro gerado.
+     * @throws {Error} - Lança o erro após logá-lo.
+    */
     const handleRequestError = (action, error) => {
         console.error(`useListTracks:${action} Error: `, error);
         throw error;
     }
 
+    /**
+     * Busca todas as faixas da tabela 'tracks' no Supabase.
+     * Atualiza o estado `fetchedTracks` com o resultado da busca.
+     * @async
+     * @returns {Promise<void>} Retorna uma Promise resolvida quando as faixas forem buscadas.
+    */
     const fetchTracks = async () => {
         try {
             const { data: tracks } = await supabase.from('tracks').select('*');
@@ -19,6 +38,12 @@ const useListTracks = () => {
         }
     }
 
+    /**
+     * Busca uma faixa específica pelo UID na tabela 'tracks' no Supabase.
+     * @async
+     * @param {string} uid - O UID da faixa a ser buscada.
+     * @returns {Promise<Object|null>} Retorna a faixa encontrada ou `null` se nenhuma faixa for encontrada.
+    */
     const fetchTrackByUid = async (uid) => {
         try {
             const { data: track } = await supabase.from('tracks').select('*').eq('uid', uid);
@@ -27,6 +52,7 @@ const useListTracks = () => {
             handleRequestError('fetchTrackByUid', error);
         }
     }
+
 
     return {
         fetchedTracks,
