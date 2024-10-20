a727dc7251c2a8934d7a2d695ba629ebe879f3b2
kbrito504 <kbrito@flashvolve.com>
2024-10-20 12:03:51 -0300

    chore: configure i18n dependencie in main.js project file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import naive from 'naive-ui';
+import i18n from './i18n';
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
@@ -10,5 +11,6 @@ const app = createApp(App);
 
 app.use(router);
 app.use(naive);
+app.use(i18n);
 
 app.mount('#app');
